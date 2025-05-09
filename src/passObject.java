bc049deafd1a1cf596cfa7454948411db41ce867
NguyenTuongHung <151506436+NguyenTuongHung@users.noreply.github.com>
2025-05-09 15:30:11 +0700

    Hung

---
+++ src/passObject.java
@@ -0,0 +1,11 @@
+public class passObject {
+    public int tuoi;
+
+    public void nhapTuoi(int x) {
+        this.tuoi = x;
+    }
+
+    public int hienthiTuoi() {
+        return this.tuoi;
+    }
+}
