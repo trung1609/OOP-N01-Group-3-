b58e37c4912bc66681242c3ed4a1b7f3fc379cbb
Trung <trung8d2005@gmail.com>
2025-05-09 13:59:46 +0700

    Confirm name

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        PassObject object = new PassObject("Hưng");
+        object.display();
+    }
+
+}
---
+++ src/PassObject.java
@@ -0,0 +1,9 @@
+public class PassObject {
+    String name;
+    PassObject(String name){
+        this.name = name;
+    }
+    public void display(){
+        System.out.println("Name: " + name);
+    }
+}
